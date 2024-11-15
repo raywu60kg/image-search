3b5c3ec54a0662ce718ff4683ea014981ca17e0b
ray <wuhaohsiang1992@gmail.com>
2024-11-15 17:36:50 +0800

    add data in db

---
+++ alembic/versions/b80c2aaff018_add_image_data.py
@@ -0,0 +1,63 @@
+"""add image data
+
+Revision ID: b80c2aaff018
+Revises: cdb794c720ae
+Create Date: 2024-11-15 16:55:22.903318
+
+"""
+
+import base64
+import os
+from typing import Sequence, Union
+
+import numpy as np
+from sqlalchemy import text
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "b80c2aaff018"
+down_revision: Union[str, None] = "cdb794c720ae"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+image_dir = "image_data/val2014"
+image_embedding_dir = "image_data/val2014_embeddings/clip.npy"
+
+
+def image_to_base64(image_path: str) -> str:
+    with open(image_path, "rb") as image_file:
+        base64_string = base64.b64encode(image_file.read()).decode("utf-8")
+    return base64_string
+
+
+def upgrade() -> None:
+    conn = op.get_bind()
+    base64_image_list = [
+        image_to_base64(os.path.join(image_dir, file_name))
+        for file_name in os.listdir(image_dir)
+    ]
+    image_embedding = np.load(image_embedding_dir, allow_pickle=True)
+
+    # Insert each vector with additional metadata
+    for i in range(len(base64_image_list)):
+        conn.execute(
+            text("""
+                INSERT INTO image ( 
+                    image_base64, 
+                    image_embedding
+                )
+                VALUES (
+                    :image_base64, 
+                    :image_embedding
+                )
+            """),
+            {
+                "image_base64": base64_image_list[i],
+                "image_embedding": image_embedding[i].tolist(),
+            },
+        )
+
+
+def downgrade() -> None:
+    conn = op.get_bind()
+    conn.execute(text("TRUNCATE TABLE your_table_name"))
